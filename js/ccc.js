129b559936099dc6e3817817edbe47bd05a1cadb
xfloyd2 <darek.gliniak@gmail.com>
2023-12-17 10:12:08 +0100

    poprawiono numery obrazków

--- js/ccc.js
+++ js/ccc.js
@@ -2,15 +2,15 @@
 //const addButton = document.getElementById("add-button");
 const btnPhoto = document.createElement("button");
 btnPhoto.innerText = "Zmien zdjęcie";
-const fieldPhoto = document.createElement("p");
+const fieldPhoto = document.createElement("div");
 document.body.appendChild(btnPhoto);
 document.body.appendChild(fieldPhoto);
 for (let z = 1; z < 4; z++) {
   btnPhoto.addEventListener("click", addRandomImg, false);
 
   function addRandomImg() {
-    let randomIndex = Math.floor(Math.random() * 6);
-
+    let randomIndex1 = Math.floor(Math.random() * 6);
+    let randomIndex = randomIndex1 + 1;
     const newImage = document.createElement("img");
     newImage.src = `image/k${randomIndex}.png`;
     fieldPhoto.appendChild(newImage);
